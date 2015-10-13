fe6862d2917796785c9c354a62ac137c255a2ed8
Arkkra <gen.rkd@hotmail.com>
2015-10-12 17:59:37 -0700

    start parsing in page_rank

--- page_rank.py
+++ page_rank.py
@@ -1,5 +1,35 @@
+import re
+import pprint
+import string
+import sys
 
-def get_data(data):
+def get_data(fp):
+	nodes = {}
+
+	data = fp.read().splitlines()
+	for line in data:
+		line = re.split(r',', line)
+		line = [col.strip(string.punctuation) for col in line]
+		nodes[line[0]] = (line[1], line[2])
+
+	#pprint.pprint(nodes)
 
 
 if __name__ == '__main__':
+	try: 
+		fname = sys.argv[1]
+	except Exception:
+		print "usage: page_rank.py <file>"
+
+	#fp = open('NCAA_football.csv', 'r')
+	#data_ncaa = get_data(fp)
+	#fp.close()
+
+
+
+	"""fp = open('stateborders.csv', 'r')
+	data_borders = get_data(fp)
+	fp.close()
+	fp = open('karate.csv', 'r')
+	data_karate = get_data(fp)"""
+
