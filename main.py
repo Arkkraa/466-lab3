4cb772ad1a205f63159dda78a7d0e8147dacf846
jlguevara <jlguevara81@gmail.com>
2015-10-21 21:52:17 -0700

    calculates football page rank

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def runStates():
 
    graph = PageRank()
    for line in f:
-      column = line.split(',')
-      left = column[0].strip('"')
-      right = column[2].strip('"')
+      columns = line.split(',')
+      left = columns[0].strip('"')
+      right = columns[2].strip('"')
       graph.addEdge(left, right)
 
    graph.printGraph()
@@ -26,5 +26,36 @@ def runStates():
    print "Number of iterations:", iterations
    print returnSorted(ranks)
 
+def runFootball():
+   """run pagerank on NCAA_football.csv"""
+
+   f = open('NCAA_football.csv')
+   graph = PageRank()
+
+   for line in f:
+      columns = line.split(',')
+      team1 = columns[0].strip()
+      value1 = int(columns[1])
+      team2 = columns[2].strip()
+      value2 = int(columns[3])
+
+      if value1 > value2:
+         graph.addEdge(team2, team1)
+
+      elif value1 < value2:
+         graph.addEdge(team1, team2)
+
+      else:
+         graph.addEdge(team2, team1)
+         graph.addEdge(team1, team2)
+
+   graph.printGraph()
+   iterations, ranks = graph.getPageRank()
+   print "Number of iterations:", iterations
+   print returnSorted(ranks)
+
+
+      
 if __name__ == '__main__':
-   runStates()
+   #runStates()
+   runFootball()
